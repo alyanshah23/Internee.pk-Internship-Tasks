b6cb25d7d7c3de0f451baeed628ce24550cc7746
alyanshah23 <alyanshah2378@gmail.com>
2026-01-29 07:49:30 -0800

    js started

---
+++ Weather App using Api Task 2/script.js
@@ -0,0 +1,42 @@
+const cityInput = document.querySelector(".city-input");
+const searchBtn = document.querySelector(".search-btn");
+const weatherInfoSection = document.querySelector(".weather-Info")
+const notFoundSelection = document.querySelector(".not-Found")
+const searchCitySection = document.querySelector("search-City")
+
+const apiKey = "1310332d62cbecbeb10acce7c398c317"
+
+searchBtn.addEventListener("click", () => {
+    if (cityInput.value.trim() != "") {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ""
+        cityInput.blur()
+    }
+})
+cityInput.addEventListener("keydown", (event) => {
+    if (event.key == "Enter" &&
+        cityInput.value.trim() != ""
+    ) {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ""
+        cityInput.blur()
+    }
+
+})
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl)
+    return response.json()
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData(`weather`, city)
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSelection)
+        return
+    }
+    console.log(weatherData)
+}
+function showDisplaySection(section) { 
+   
+}
